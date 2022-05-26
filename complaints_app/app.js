73c520b4514f05fa65dbbf8e5f73127cda5a64eb
Eliza Li <lie2@bxscience.edu>
2022-05-26 14:49:39 -0400

    edit

--- complaints_app/app.js
+++ complaints_app/app.js
@@ -6,13 +6,7 @@ fetch('https://data.cityofnewyork.us/resource/cwy2-px8b.json')
 })
 
 .catch(err => console.log(err))
-// fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
-// .then(response => response.json()) //parting res to json
-// .then(users => {
-//     console.log(users);
-// })
-// // .then(json => console.log(json)) //json data
-// .catch(err => console.log(err)) //handle errors
+
 
 const manhattan = document.getElementById('manhattan')
 const brooklyn = document.getElementById('brooklyn')
